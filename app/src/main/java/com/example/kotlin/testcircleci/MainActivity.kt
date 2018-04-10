2b12219f77351c84b1936b85181b467f3332a0ef
Phuc Tran <phuc.tran@wizeline.com>
2018-04-10 15:14:18 +0700

    Add fastlane

--- app/src/main/java/com/example/kotlin/testcircleci/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/testcircleci/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.kotlin.testcircleci
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
     }
 }
